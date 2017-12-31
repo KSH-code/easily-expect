ec0ca8b86330a95968ffc7dfc893cddde02e1ba5
ksh-code <tjdgnsqn133@gmail.com>
2017-12-31 14:21:00 +0900

    make checkEqual code

---
+++ test/checkEqual.test.js
@@ -0,0 +1,18 @@
+/* global describe, it */
+'use strict'
+const { checkEqual } = require('../index')
+
+describe('Test Equal', () => {
+  it('String Type Test', () => {
+    checkEqual('a', 'a', `a`/* , 'b' */)
+  })
+  it('Number Type Test', () => {
+    checkEqual(1, 1/* , 2 */)
+  })
+  it('Object Type Test', () => {
+    checkEqual({}, {}/* , { a: 1 } */)
+  })
+  it('null Type Test', () => {
+    checkEqual(null, null/* , NaN */)
+  })
+})
