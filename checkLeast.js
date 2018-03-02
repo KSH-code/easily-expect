a399216415afa924049f275a39cc5e2320b775d4
KSH-code <tjdgnsqn133@gmail.com>
2018-03-02 12:58:47 +0900

    add checkWithin
    close #16

--- checkLeast.js
+++ checkLeast.js
@@ -6,7 +6,7 @@ const { expect } = require('chai')
  * @param {Number} number
  * @param {Objects} objects
  */
-module.exports = function checkWithin (number, ...objects) {
+module.exports = function checkLeast (number, ...objects) {
   for (let i = 0; i < objects.length; i++) {
     expect(objects[i]).to.be.at.least(number)
   }
